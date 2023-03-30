34d3a5ec6d5303be7a6be51ca44094570a2b4205
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-03-30 23:18:09 +0200

    test look of pizzas

--- src/pages/MainContent.vue
+++ src/pages/MainContent.vue
@@ -4,7 +4,33 @@
   <p>Sekcja 3</p>
 
   <ul>
-    <li v-for="pizza in filteredPizzas" :key="pizza.id">{{ pizza.name }}</li>
+    <li v-for="pizza in filteredPizzas" :key="pizza.id">
+      <div class="xd">
+        <h3>Nazwa</h3>
+        <p>{{ pizza.name }}</p>
+      </div>
+      <div class="xd">
+        <h3>Składniki</h3>
+        <p>{{ pizza.ingredients }}</p>
+      </div>
+      <div class="xd">
+        <h4>24 cm {{ pizza.priceSmall }}</h4>
+        <p>{{ pizza.priceSmall }} zł</p>
+        <input type="radio" />
+      </div>
+
+      <div class="xd">
+        <h3>30 cm</h3>
+        <p>{{ pizza.priceMedium }} zł</p>
+        <input type="radio" />
+      </div>
+      <div class="xd">
+        <h3>45 cm</h3>
+        <p>{{ pizza.priceBig }} zł</p>
+        <input type="radio" />
+      </div>
+      <button>Dodaj do koszyka</button>
+    </li>
   </ul>
 </template>
 
@@ -17,3 +43,23 @@ export default {
   },
 };
 </script>
+<style>
+li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+div.xd {
+  flex: 1;
+  flex-direction: column;
+  margin-right: 20px;
+}
+p {
+  flex: 1;
+  align-items: center;
+}
+h3 {
+  justify-content: start;
+  align-items: start;
+}
+</style>
