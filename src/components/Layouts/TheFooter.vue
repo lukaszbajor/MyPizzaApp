38e6cbe7d38944115f2d4ebca9c12a248529eed0
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-04-01 22:11:19 +0200

    Register form first code lines

--- src/components/Layouts/TheFooter.vue
+++ src/components/Layouts/TheFooter.vue
@@ -3,3 +3,9 @@
     <p>Stopka</p>
   </footer>
 </template>
+<style>
+footer {
+  padding: 10px;
+  background-color: tomato;
+}
+</style>
--- src/pages/RegisterView.vue
+++ src/pages/RegisterView.vue
@@ -1,3 +1,74 @@
 <template>
-  <p>Rejestracja w serwisie.</p>
+  <form @submit.prevent="submit">
+    <label for="firstName">Imię:</label>
+    <input
+      type="text"
+      name="firstName"
+      id="firstName"
+      v-model="userFirstName"
+    />
+    <label for="lastName">Nazwisko:</label>
+    <input type="text" name="lastName" id="lastName" v-model="userLastName" />
+    <label for="city">Miejscowość:</label>
+    <input type="text" name="city" id="city" v-model="userCity" />
+    <label for="street">Ulica:</label>
+    <input type="text" name="street" id="street" v-model="userStreet" />
+    <label for="homeNumber">Numer domu/mieszkania:</label>
+    <input
+      type="text"
+      name="homeNumber"
+      id="homeNumber"
+      v-model="userHomeNumber"
+    />
+    <label for="phoneNumber">Numer telefonu:</label>
+    <input
+      type="number"
+      name="phoneNumber"
+      id="phoneNumber"
+      v-model="userPhoneNumber"
+    />
+    <label for="login">Login:</label>
+    <input type="text" name="login" id="login" v-model="userLogin" />
+    <label for="password">Hasło:</label>
+    <input
+      type="password"
+      name="password"
+      id="password"
+      v-model="userPassword"
+    />
+    <label for="password2">Powtórz hasło:</label>
+    <input
+      type="password"
+      name="password2"
+      id="password2"
+      v-model="userPassword2"
+    />
+    <input type="submit" value="Zarejestruj się" />
+    <input type="reset" value="Resetuj" />
+  </form>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      userFirstName: "",
+      userLastName: "",
+      userCity: "",
+      userStreet: "",
+      userHomeNumber: "",
+      userPhoneNumber: "",
+      userLogin: "",
+      userPassword: "",
+      userPassword2: "",
+    };
+  },
+  methods: {
+    submit() {
+      alert(
+        this.userLogin + " " + this.userPassword + " " + this.userHomeNumber
+      );
+    },
+  },
+};
+</script>
+<style></style>
