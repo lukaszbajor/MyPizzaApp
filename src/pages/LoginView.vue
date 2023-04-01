1e793db4465082d22875e004299a88ce68c74e57
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-04-01 21:50:51 +0200

    Login form first code lines

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>To jest strona logowania.</p>
+  <form @submit.prevent="submit">
+    <label for="login">Login:</label>
+    <input type="text" name="login" id="login" v-model="userLogin" />
+    <label for="password">Hasło:</label>
+    <input
+      type="password"
+      name="password"
+      id="password"
+      v-model="userPassword"
+    />
+    <input type="submit" value="Zaloguj się" />
+    <input type="reset" value="Resetuj" />
+  </form>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      userLogin: "",
+      userPassword: "",
+    };
+  },
+  methods: {
+    submit() {
+      alert(this.userLogin + " " + this.userPassword);
+    },
+  },
+};
+</script>
+<style></style>
