3f4bfacdbdf0913aac9e8da81a036d85e45745c8
lukaszbajor <48011715+lukaszbajor@users.noreply.github.com>
2023-04-03 22:18:48 +0200

    input radio problem - solved (upgrade)

--- src/components/pizzas/PizzaItem.vue
+++ src/components/pizzas/PizzaItem.vue
@@ -11,19 +11,19 @@
     <div class="xd">
       <h4>24 cm {{ pizza.priceSmall }}</h4>
       <p>{{ pizza.priceSmall }} zł</p>
-      <input type="radio" name="pizza.id" />
+      <input type="radio" name="pizza" />
     </div>
 
     <div class="xd">
       <h3>30 cm</h3>
       <p>{{ pizza.priceMedium }} zł</p>
-      <input type="radio" name="pizza.id" />
+      <input type="radio" name="pizza" />
     </div>
     <div class="xd">
       <h3>45 cm</h3>
       <p>{{ pizza.priceBig }} zł</p>
 
-      <input type="radio" name="pizza.id" />
+      <input type="radio" name="pizza" />
     </div>
     <button>Dodaj do koszyka</button>
   </li>
